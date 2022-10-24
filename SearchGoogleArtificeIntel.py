bab29685bd9448e8b43c926bb31bd2dee469c890
SMedd69 <97759843+SMedd69@users.noreply.github.com>
2022-10-24 03:38:12 +0200

    Update SearchGoogleArtificeIntel.py

--- SearchGoogleArtificeIntel.py
+++ SearchGoogleArtificeIntel.py
@@ -1,8 +1,8 @@
-import pywhatkit as pw
+import pywhatkit as pw //importation de la librairie pywhatkit que je nomme pw
 
 try:
-    pw.search("Said Meddahi")
-    print("Recherche...")
+    pw.search("pywhatkit") //Tapez le(s) mot(s) clé(s) de votre recherche
+    print("Recherche...") //Texte qui affiche la recherche qui est effectué
     
 except:
-    print("An unknown error occurred")
+    print("An unknown error occurred") // Texte à afficher en cas d erreurs
