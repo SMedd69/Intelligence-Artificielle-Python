de464c684f457126c2b332960424907d2e156cb2
SMedd69 <s.meddahi.fo@gmail.com>
2022-10-24 03:34:32 +0200

    Lire voix artificielle via un texte

---
+++ VoixIntelArtificielle.py
@@ -0,0 +1,7 @@
+import pyttsx3 as vai
+
+engine=vai.init()
+
+engine.say('La purge vas bientôt commencer. Veuillez vous préparez.. 3  2  1  biiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiip !!')
+
+engine.runAndWait()
